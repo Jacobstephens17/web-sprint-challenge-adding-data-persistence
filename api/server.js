c52c0b5662c31bada4679e0fb226f774fd8254b9
Jacob Stephens <jacobstephens2@gmail.com>
2021-02-19 02:04:49 -0800

    initial commit, server set up

--- api/server.js
+++ api/server.js
@@ -1,1 +1,15 @@
 // build your server here and require it from index.js
+const express = require('express');
+const server = express()
+
+
+const ProjectRouter = require('/api/project/router')
+const ResourceRouter = require('/api/resource/router')
+const TaskRouter = require('/api/task/router')
+
+
+
+server.use(express.json())
+server.use('/api/project', ProjectRouter)
+server.use('/api/resource', ResourceRouter)
+server.use('/api/task', TaskRouter)
--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,30 @@
-// do not make changes to this file (except to optionally add seeds)
-const sharedConfig = {
-  client: 'sqlite3',
-  useNullAsDefault: true,
-  migrations: { directory: './data/migrations' },
-  pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
-}
+// Update with your config settings.
 
 module.exports = {
+
   development: {
-    ...sharedConfig,
-    connection: { filename: './data/lambda.db3' },
+    client: 'sqlite3',
+    useNullAsDefault: true, 
+    connection: {
+      filename: './dev.sqlite3'
+    }
   },
-  testing: {
-    ...sharedConfig,
-    connection: { filename: './data/test.db3' },
-  },
+
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
 };
